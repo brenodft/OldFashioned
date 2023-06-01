d485456feca7ef30bcfdefd45de89729fc65e6dc
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2023-05-31 22:37:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const favoriteButton1 = document.getElementById('favorite-button-1');
+const favoriteButton2 = document.getElementById('favorite-button-2');
+const favoriteButton3 = document.getElementById('favorite-button-3');
+const favoriteButton4 = document.getElementById('favorite-button-4');
+const favoriteButton5 = document.getElementById('favorite-button-5');
+const favoriteButton6 = document.getElementById('favorite-button-6');
+const favoriteButton7 = document.getElementById('favorite-button-7');
+const favoriteButton8 = document.getElementById('favorite-button-8');
+const favoriteButton9 = document.getElementById('favorite-button-9');
+
+favoriteButton1.addEventListener('click', function() {
+  favoriteButton1.classList.toggle('favorited');
+});
+
+favoriteButton2.addEventListener('click', function() {
+  favoriteButton2.classList.toggle('favorited');
+});
+favoriteButton3.addEventListener('click', function() {
+  favoriteButton3.classList.toggle('favorited');
+});
+favoriteButton4.addEventListener('click', function() {
+  favoriteButton4.classList.toggle('favorited');
+});
+favoriteButton5.addEventListener('click', function() {
+  favoriteButton5.classList.toggle('favorited');
+});
+favoriteButton6.addEventListener('click', function() {
+  favoriteButton6.classList.toggle('favorited');
+});
+favoriteButton7.addEventListener('click', function() {
+  favoriteButton7.classList.toggle('favorited');
+});
+favoriteButton8.addEventListener('click', function() {
+  favoriteButton8.classList.toggle('favorited');
+});
+favoriteButton9.addEventListener('click', function() {
+  favoriteButton9.classList.toggle('favorited');
+});
+
+
